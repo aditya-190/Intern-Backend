934585f9ed484650d1c9147fe33e0989dfc57da4
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2022-02-23 00:00:56 +0530

    Bug Fixes.

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import sys
 
 import intern.spiders.Linkedin as linkedinSpider
 
-linkedinSpider.main(number_of_pages=sys.argv[1], keywords=sys.argv[2], location=sys.argv[3], mode=sys.argv[4])
+linkedinSpider.main(number_of_pages=int(sys.argv[1]), keywords=sys.argv[2], location=sys.argv[3], mode=sys.argv[4])
