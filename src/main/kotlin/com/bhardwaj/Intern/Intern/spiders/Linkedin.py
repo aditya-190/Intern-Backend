4d27e4d626c64befb5020fd812181fa88f262f7f
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2022-02-22 10:18:14 +0530

    Checking New Jobs in Production Mode.

--- src/main/kotlin/com/bhardwaj/Intern/Intern/spiders/Linkedin.py
+++ src/main/kotlin/com/bhardwaj/Intern/Intern/spiders/Linkedin.py
@@ -84,10 +84,10 @@ def send_data():
     production_url = "https://aditya-intern-backend.herokuapp.com/job/all"
     development_url = "http://0.0.0.0:8080/job/all"
 
-    base_url = development_url
+    base_url = production_url
     headers = {
         "Content-Type": "application/json; charset=utf-8",
-        "Authorization": "Bearer {TOKEN HERE}"
+        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwOi8vMC4wLjAuMDo4MDgwL2hlbGxvIiwiaXNzIjoiaHR0cDovLzAuMC4wLjA6ODA4MC8iLCJlbWFpbCI6ImFhZGkuYmJoYXJkd2FqQGdtYWlsLmNvbSJ9.3jdo9WUyeASv9GbTWHjRPjLrk5sg0cCKgzcMcC5EF4w"
     }
     json_data = json.load(open('output.json'))
     requests.post(base_url, headers=headers, json=json_data)
