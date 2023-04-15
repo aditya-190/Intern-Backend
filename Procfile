6917a0c2cde8e2f3968656c386fc24217d2e099b
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2023-04-15 15:52:04 +0530

    Code cleanup.

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: build/install/com.bhardwaj.intern-backend/bin/com.bhardwaj.intern-backend
-worker: python main.py
--- main.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-import intern.spiders.Linkedin as linkedinSpider
-
-linkedinSpider.main(number_of_pages=int(sys.argv[1]), keywords=sys.argv[2], location=sys.argv[3], mode=sys.argv[4])
